fdf142e75d2e36a6a3cb051bebadaffca4662927
Kushagra Singhal <kushagrasinghal901@gmail.com>
2026-06-06 01:47:45 +0530

    Update timer.c

--- pbl/timer.c
+++ pbl/timer.c
@@ -5,6 +5,6 @@ double measureTime(SortFunction fn, int* arr, int n, Metrics* m) {
     clock_t start = clock();
     fn(arr, n, m);
     clock_t end = clock();
-    double elapsed = ((double)(end - start) / CLOCKS_PER_SEC) * 1000.0;  // convert to milliseconds
+    double elapsed = ((double)(end - start) / CLOCKS_PER_SEC) * 1000.0; 
     return elapsed;
 }
